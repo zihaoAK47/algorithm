e5c2424742e6f969f400bad663323a079528bf24
niugiaogiao <zihaoAK47@163.com>
2022-07-31 14:24:14 +0800

    fix: lc.078 add impl.

--- src/java/com/niugiaogiao/linked/leetcode/LinkedList078.java
+++ src/java/com/niugiaogiao/linked/leetcode/LinkedList078.java
@@ -47,4 +47,37 @@ public class LinkedList078 {
 
         return newHead.next;
     }
+
+    public ListNode mergeKLists1(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        int mid = start + ((end - start) >> 1);
+        return mergeRun(merge(lists, start, mid), merge(lists, mid + 1, end));
+    }
+
+    public ListNode mergeRun(ListNode nodeA, ListNode nodeB) {
+        ListNode pre = new ListNode(0);
+        ListNode backPre = pre;
+        while (nodeA != null && nodeB != null) {
+            if (nodeA.val < nodeB.val) {
+                backPre.next = nodeA;
+                nodeA = nodeA.next;
+            } else {
+                backPre.next = nodeB;
+                nodeB = nodeB.next;
+            }
+            backPre = backPre.next;
+        }
+        backPre.next = nodeA != null ? nodeA : nodeB;
+        return pre.next;
+    }
 }
