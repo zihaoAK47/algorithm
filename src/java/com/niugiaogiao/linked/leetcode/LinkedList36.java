a972de31c0777b198dc2348beaedcb0cc3b0ed69
niugiaogiao <zihaoAK47@163.com>
2022-08-12 13:55:04 +0800

    fix: lc.36 add describe

--- src/java/com/niugiaogiao/linked/leetcode/LinkedList36.java
+++ src/java/com/niugiaogiao/linked/leetcode/LinkedList36.java
@@ -1,5 +1,10 @@
 package com.niugiaogiao.linked.leetcode;
 
+/**
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。
+ *
+ * @author zihao
+ */
 public class LinkedList36 {
 
     static class Node {
