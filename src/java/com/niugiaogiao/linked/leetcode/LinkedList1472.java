d1739a540420b292ceb1c341205650285afc6ace
niugiaogiao <zihaoAK47@163.com>
2022-08-23 22:37:06 +0800

    Add impl lc.1472

--- src/java/com/niugiaogiao/linked/leetcode/LinkedList1472.java
+++ src/java/com/niugiaogiao/linked/leetcode/LinkedList1472.java
@@ -1,5 +1,6 @@
 package com.niugiaogiao.linked.leetcode;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 /**
@@ -179,6 +180,45 @@ public class LinkedList1472 {
         }
     }
 
+    static class BrowserHistory4 {
+        Deque<String> s1 = new LinkedList<>();
+        Deque<String> s2 = new LinkedList<>();
+
+        public BrowserHistory4(String homepage) {
+            if (isEmpty((homepage))) {
+                return;
+            }
+            s1.push(homepage);
+        }
+
+        public void visit(String url) {
+            if (isEmpty((url))) {
+                return;
+            }
+            s1.push(url);
+            s2.clear();
+        }
+
+        public String back(int steps) {
+            for (int i = 0 ; i < steps && s1.size() > 1 ; i ++) {
+                s2.push(s1.pop());
+            }
+
+            return s1.peek();
+        }
+
+        public String forward(int steps) {
+            for (int i = 0 ; i < steps && !s2.isEmpty() ; i++) {
+                s1.push(s2.pop());
+            }
+
+            return s1.peek();
+        }
+
+        public boolean isEmpty(String page) {
+            return page == null || page.length() == 0;
+        }
+    }
 
     public static void main(String[] args) {
         BrowserHistory3 browserHistory = new BrowserHistory3("leetcode.com");
