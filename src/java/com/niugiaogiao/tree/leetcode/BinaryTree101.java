912c4de436f3dc0359767f11ec1be3755b330433
niugiaogiao <zihaoAK47@163.com>
2022-08-24 23:25:47 +0800

    Add impl lc.101

--- src/java/com/niugiaogiao/tree/leetcode/BinaryTree101.java
+++ src/java/com/niugiaogiao/tree/leetcode/BinaryTree101.java
@@ -24,7 +24,7 @@ public class BinaryTree101 {
         }
     }
 
-    public static boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric(TreeNode root) {
         Deque<TreeNode> left = new LinkedList<>();
         Deque<TreeNode> right = new LinkedList<>();
         left.addFirst(root);
@@ -44,10 +44,17 @@ public class BinaryTree101 {
                 return false;
             }
             if (leftNode.left != null && rightNode.right != null) {
+                if (leftNode.left.val != rightNode.right.val) {
+                    return false;
+                }
+
                 left.add(leftNode.left);
                 right.add(rightNode.right);
             }
             if (leftNode.right != null && rightNode.left != null) {
+                if (leftNode.right.val != rightNode.left.val) {
+                    return false;
+                }
                 left.add(leftNode.right);
                 right.add(rightNode.left);
             }
@@ -56,15 +63,54 @@ public class BinaryTree101 {
         return left.isEmpty() && right.isEmpty();
     }
 
-    public static void main(String[] args) {
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode();
-        TreeNode t3 = new TreeNode();
-        TreeNode t4 = new TreeNode();
-        TreeNode t5 = new TreeNode();
-        TreeNode t6 = new TreeNode();
-        TreeNode t7 = new TreeNode();
+    public static boolean isSymmetric1(TreeNode root) {
+        return run(root, root);
+    }
 
-        System.err.println(isSymmetric(t1));
+
+    public static boolean run(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+
+        boolean res1 = false;
+        if (leftNode.left != null && rightNode.right != null) {
+            if (leftNode.left.val != rightNode.right.val) {
+                res1 = false;
+            } else {
+                res1 = run(leftNode.left, rightNode.right);
+            }
+        } else if (leftNode.left == null && rightNode.right == null) {
+            res1 = true;
+        }
+
+        boolean res2 = false;
+        if (leftNode.right != null && rightNode.left != null) {
+            if (leftNode.right.val != rightNode.left.val) {
+                res2 = false;
+            } else {
+                res2 = run(leftNode.right, rightNode.left);
+            }
+        } else if (leftNode.right == null && rightNode.left == null) {
+            res2 = true;
+        }
+
+        return res1 && res2;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(2);
+        TreeNode t2 = new TreeNode(3);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(5);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+        t3.left = t6;
+
+        System.err.println(isSymmetric1(t1));
     }
 }
