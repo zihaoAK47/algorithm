60e3b6b97bad130abcfbd37f00e1183a817663b6
niugiaogiao <zihaoAK47@163.com>
2022-08-26 18:34:25 +0800

    Add impl lc.102

--- src/java/com/niugiaogiao/tree/leetcode/BinaryTree102.java
+++ src/java/com/niugiaogiao/tree/leetcode/BinaryTree102.java
@@ -1,8 +1,6 @@
 package com.niugiaogiao.tree.leetcode;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）
@@ -40,7 +38,7 @@ public class BinaryTree102 {
         while (!queue.isEmpty()) {
             List<Integer> itemResult = new LinkedList<>();
             int size = queue.size();
-            for (int i = 0 ; i < size && !queue.isEmpty() ; i++) {
+            for (int i = 0; i < size && !queue.isEmpty(); i++) {
                 TreeNode nodeItem = queue.poll();
                 itemResult.add(nodeItem.val);
                 if (nodeItem.left != null) {
@@ -52,7 +50,47 @@ public class BinaryTree102 {
             }
             result.add(itemResult);
         }
-
         return result;
+    }
+
+    static List<List<Integer>> res = new ArrayList<>();
+
+    public static List<List<Integer>> levelOrder1(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        run(root, 1);
+        return res;
+    }
+
+    public static void run(TreeNode node, int index) {
+        if (node == null) {
+            return;
+        }
+        if (res.size() < index) {
+            res.add(index - 1, new ArrayList<>());
+        }
+        res.get(index - 1).add(node.val);
+        run(node.left, index + 1);
+        run(node.right, index + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode r1 = new TreeNode(1);
+        TreeNode r2 = new TreeNode(2);
+        TreeNode r3 = new TreeNode(3);
+        TreeNode r4 = new TreeNode(4);
+        TreeNode r5 = new TreeNode(5);
+        TreeNode r6 = new TreeNode(6);
+        TreeNode r7 = new TreeNode(7);
+
+        r1.left = r2;
+        r1.right = r3;
+        r2.left = r4;
+        r2.right = r5;
+        r3.left = r6;
+        r3.right = r7;
+        levelOrder1(r1);
+        System.err.println("");
     }
 }
