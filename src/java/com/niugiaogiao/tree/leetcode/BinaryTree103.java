f11e30676d64407c483afe28687646adfb398f25
niugiaogiao <zihaoAK47@163.com>
2022-08-27 21:12:03 +0800

    Ac lc.103

---
+++ src/java/com/niugiaogiao/tree/leetcode/BinaryTree103.java
@@ -0,0 +1,86 @@
+package com.niugiaogiao.tree.leetcode;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）
+ *
+ * @author zi hao
+ * @version 1.0
+ * @date 2022-08-27 20:27
+ */
+public class BinaryTree103 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            List<Integer> itemNodes = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = flag ? queue.pollFirst() : queue.pollLast();
+                itemNodes.add(treeNode.val);
+                if (flag) {
+                    if (treeNode.left != null) {
+                        queue.addLast(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        queue.addLast(treeNode.right);
+                    }
+                } else {
+                    if (treeNode.right != null) {
+                        queue.addFirst(treeNode.right);
+                    }
+                    if (treeNode.left != null) {
+                        queue.addFirst(treeNode.left);
+                    }
+                }
+            }
+            flag = !flag;
+            res.add(itemNodes);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t3.right = t5;
+        zigzagLevelOrder(t1);
+    }
+}
