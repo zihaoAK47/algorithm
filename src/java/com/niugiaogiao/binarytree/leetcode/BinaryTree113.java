9c71b527c633b7b1e175722c8928466398c14681
niugiaogiao <zihaoAK47@163.com>
2022-09-02 14:01:58 +0800

    Ac lc.113

---
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree113.java
@@ -0,0 +1,75 @@
+package com.niugiaogiao.binarytree.leetcode;
+
+import java.util.*;
+
+/**
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ * 叶子节点 是指没有子节点的节点
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/path-sum-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class BinaryTree113 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> pathSumBFS(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queueNode = new LinkedList<>();
+        Queue<Integer> queueVal = new LinkedList<>();
+        queueNode.add(root);
+        queueVal.add(root.val);
+
+        Map<TreeNode, TreeNode> dict = new HashMap<>();
+        dict.put(root, null);
+        while (!queueNode.isEmpty()) {
+            TreeNode pollNode = queueNode.poll();
+            Integer pollVal = queueVal.poll();
+            if (pollNode.left == null && pollNode.right == null) {
+                if (pollVal == targetSum) {
+                    LinkedList<Integer> path = new LinkedList<>();
+                    path.addFirst(pollNode.val);
+                    TreeNode temp = pollNode;
+                    while ((temp = dict.get(temp)) != null) {
+                        path.addFirst(temp.val);
+                    }
+                    res.add(path);
+                }
+                continue;
+            }
+            if (pollNode.left != null) {
+                dict.put(pollNode.left, pollNode);
+                queueNode.add(pollNode.left);
+                queueVal.add(pollNode.left.val + pollVal);
+            }
+            if (pollNode.right != null) {
+                dict.put(pollNode.right, pollNode);
+                queueNode.add(pollNode.right);
+                queueVal.add(pollNode.right.val + pollVal);
+            }
+        }
+
+        return res;
+    }
+}
