1c924936c6c695cf90c6a716462eaf7114f7ff60
niugiaogiao <zihaoAK47@163.com>
2022-09-06 15:54:50 +0800

    Add topic describes lc.101

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree101.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree101.java
@@ -4,6 +4,9 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ */
 public class BinaryTree101 {
 
     public static class TreeNode {
