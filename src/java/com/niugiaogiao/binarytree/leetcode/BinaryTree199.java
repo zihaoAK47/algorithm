f0a37650f0a7c91bed9a9e7748f86c701db9077a
niugiaogiao <zihaoAK47@163.com>
2022-09-07 09:56:23 +0800

    Add impl lc.199

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree199.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree199.java
@@ -4,6 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * https://leetcode.cn/problems/binary-tree-right-side-view/
+ */
 public class BinaryTree199 {
 
     public static class TreeNode {
