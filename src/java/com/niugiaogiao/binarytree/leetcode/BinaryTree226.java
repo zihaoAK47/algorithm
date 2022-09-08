72c9de3f0ccc6c2d0b8e554cdef8a4424e9acab6
niugiaogiao <zihaoAK47@163.com>
2022-09-08 19:00:03 +0800

    Ac lc.226

---
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree226.java
@@ -0,0 +1,52 @@
+package com.niugiaogiao.binarytree.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点
+ * https://leetcode.cn/problems/invert-binary-tree/
+ */
+public class BinaryTree226 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode itemNode = queue.poll();
+            TreeNode temp = itemNode.left;
+            itemNode.left = itemNode.right;
+            itemNode.right = temp;
+            if (itemNode.left != null) {
+                queue.add(itemNode.left);
+            }
+            if (itemNode.right != null) {
+                queue.add(itemNode.right);
+            }
+        }
+
+        return root;
+    }
+}
