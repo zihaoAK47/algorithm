076c790f8bc87fd92a141b3c2d5ee1b21cecad05
niugiaogiao <zihaoAK47@163.com>
2022-09-10 12:27:58 +0800

    Ac lc.404

---
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree404.java
@@ -0,0 +1,84 @@
+package com.niugiaogiao.binarytree.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author zi hao
+ * @version 1.0
+ * @date 2022-09-10 12:17
+ */
+public class BinaryTree404 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int sumOfLeftLeaves(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            TreeNode item = queue.poll();
+            if (item.left != null) {
+                if (item.left.left == null && item.left.right == null) {
+                    res += item.left.val;
+                }
+                queue.add(item.left);
+            }
+            if (item.right != null) {
+                queue.add(item.right);
+            }
+        }
+
+        return res;
+    }
+
+    int res = 0;
+    public int sumOfLeftLeaves1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        run(root);
+        return res;
+    }
+
+    public void run(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (node.left != null && node.left.left == null && node.left.right == null) {
+            res += node.left.val;
+        }
+        run(node.left);
+        run(node.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(9);
+        TreeNode t3 = new TreeNode(20);
+        TreeNode t4 = new TreeNode(15);
+        TreeNode t5 = new TreeNode(7);
+        t1.left = t2;
+        t1.right = t3;
+        t3.left = t4;
+        t3.right = t5;
+        sumOfLeftLeaves(t1);
+    }
+}
