78744734d19bf40b322b4cf903266c1311cba182
niugiaogiao <zihaoAK47@163.com>
2022-09-14 10:11:40 +0800

    Add impl lc.257

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree257.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree257.java
@@ -2,6 +2,8 @@ package com.niugiaogiao.binarytree.leetcode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree257 {
 
@@ -24,7 +26,7 @@ public class BinaryTree257 {
         }
     }
 
-    public static List<String> binaryTreePaths(TreeNode root) {
+    public static List<String> binaryTreePathsDFS1(TreeNode root) {
         runDfs(root);
         return null;
     }
@@ -38,7 +40,6 @@ public class BinaryTree257 {
         temp.add(node.val + "");
         runDfs(node.left);
         runDfs(node.right);
-
         if (node.left == null && node.right == null) {
             StringBuilder stringBuilder = new StringBuilder();
             for (int i = 0; i < temp.size() - 1; i++) {
@@ -50,6 +51,64 @@ public class BinaryTree257 {
         temp.remove(temp.size() - 1);
     }
 
+    public static String getPath(List<String> path) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < path.size() - 1; ++i) {
+            stringBuilder.append(path.get(i)).append("->");
+        }
+        stringBuilder.append(path.get(path.size() - 1));
+        return stringBuilder.toString();
+    }
+
+    List<String> res1 = new LinkedList<>();
+    public List<String> binaryTreePathsDFS2(TreeNode root) {
+        run(root,"");
+        return res;
+    }
+
+    public void run(TreeNode node, String path) {
+        if (node == null) return;
+
+        StringBuilder sb = new StringBuilder(path);
+        sb.append(node.val);
+        if (node.left == null && node.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            run(node.left, sb.toString());
+            run(node.right, sb.toString());
+        }
+    }
+
+    public static List<String> binaryTreePathsBFS(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<StringBuilder> path = new LinkedList<>();
+        List<String> res = new LinkedList<>();
+        queue.add(root);
+        path.add(new StringBuilder());
+        while (!queue.isEmpty()) {
+            TreeNode itemNode = queue.poll();
+            StringBuilder itemPath = path.poll();
+            itemPath.append(itemNode.val);
+            if (itemNode.left == null && itemNode.right == null) {
+                res.add(itemPath.toString());
+                continue;
+            }
+            itemPath.append("->");
+            if (itemNode.left != null) {
+                path.add(new StringBuilder(itemPath.toString()));
+                queue.add(itemNode.left);
+            }
+            if (itemNode.right != null) {
+                path.add(new StringBuilder(itemPath.toString()));
+                queue.add(itemNode.right);
+            }
+        }
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
         TreeNode t1 = new TreeNode(1);
         TreeNode t2 = new TreeNode(2);
@@ -58,7 +117,7 @@ public class BinaryTree257 {
         t1.left = t2;
         t1.right = t3;
         t2.right = t4;
-        binaryTreePaths(t1);
+        binaryTreePathsBFS(t1);
         System.err.println();
     }
 }
