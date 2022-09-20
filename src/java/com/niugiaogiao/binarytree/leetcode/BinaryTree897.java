e1613165f14e53e251322cef6893b123605da8e2
niugiaogiao <zihaoAK47@163.com>
2022-09-20 11:03:01 +0800

    Format code lc.897

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree897.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree897.java
@@ -3,7 +3,7 @@ package com.niugiaogiao.binarytree.leetcode;
 import java.util.Stack;
 
 /**
- * 给你一棵二叉搜索树的 root ，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树
+ * 给你一棵二叉搜索树的 root ，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树
  * 使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
  * <p>
  * 链接：https://leetcode.cn/problems/increasing-order-search-tree
