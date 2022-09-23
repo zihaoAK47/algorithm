d7de5a1fc732d6f3978f0ccb89a854ec35ceb491
niugiaogiao <zihaoAK47@163.com>
2022-09-23 10:47:25 +0800

    Format code.

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree700.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree700.java
@@ -1,9 +1,9 @@
 package com.niugiaogiao.binarytree.leetcode;
 
 /**
- * 给定二叉搜索树（BST）的根节点 root 和一个整数值 val。
+ * 给定二叉搜索树（BST）的根节点root和一个整数值val。
  * <p>
- * 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null
+ * 你需要在 BST 中找到节点值等于val的节点。 返回以该节点为根的子树。 如果节点不存在，则返回null
  * <p>
  * 链接：https://leetcode.cn/problems/search-in-a-binary-search-tree
  *
