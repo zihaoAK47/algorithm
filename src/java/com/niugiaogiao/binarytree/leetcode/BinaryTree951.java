153692b00eb39c9124c1a324777a84d8fd96382d
niugiaogiao <zihaoAK47@163.com>
2022-09-28 13:06:40 +0800

    Add impl lc.951

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree951.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree951.java
@@ -1,5 +1,8 @@
 package com.niugiaogiao.binarytree.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 951. 翻转等价二叉树
  * <p>
@@ -34,11 +37,81 @@ public class BinaryTree951 {
         }
     }
 
-    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+    public boolean flipEquiv1(TreeNode root1, TreeNode root2) {
         if (root1 == root2) return true;
         if (root1 == null || root2 == null || root1.val != root2.val) return false;
-        return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
-                || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+        return flipEquiv1(root1.left, root2.left) && flipEquiv1(root1.right, root2.right)
+                || flipEquiv1(root1.left, root2.right) && flipEquiv1(root1.right, root2.left);
+    }
+
+    public static boolean flipEquiv2(TreeNode root1, TreeNode root2) {
+        List<Integer> r1 = new ArrayList<>();
+        List<Integer> r2 = new ArrayList<>();
+        dfs2(root1, r1);
+        dfs2(root2, r2);
+        return r1.equals(r2);
+    }
+
+    public static void dfs2(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        list.add(node.val);
+        int L = node.left != null ? node.left.val : -1;
+        int R = node.right != null ? node.right.val : -1;
+        if (L < R) {
+            dfs2(node.left, list);
+            dfs2(node.right, list);
+        } else {
+            dfs2(node.right, list);
+            dfs2(node.left, list);
+        }
+        list.add(null);
+    }
+
+    public static void main(String[] args) {
+//        TreeNode t1 = new TreeNode(1);
+//        TreeNode t2 = new TreeNode(2);
+//        TreeNode t3 = new TreeNode(3);
+//        TreeNode t4 = new TreeNode(4);
+//        TreeNode t5 = new TreeNode(5);
+//        TreeNode t6 = new TreeNode(6);
+//        TreeNode t7 = new TreeNode(7);
+//        TreeNode t8 = new TreeNode(8);
+//        t1.left = t2;
+//        t1.right = t3;
+//        t2.left = t4;
+//        t2.right = t5;
+//        t5.left = t7;
+//        t5.right = t8;
+//        t3.left = t6;
+//
+//        TreeNode r1 = new TreeNode(1);
+//        TreeNode r2 = new TreeNode(3);
+//        TreeNode r3 = new TreeNode(2);
+//        TreeNode r4 = new TreeNode(6);
+//        TreeNode r5 = new TreeNode(4);
+//        TreeNode r6 = new TreeNode(5);
+//        TreeNode r7 = new TreeNode(8);
+//        TreeNode r8 = new TreeNode(7);
+//        r1.left = r2;
+//        r1.right = r3;
+//        r2.right = r4;
+//        r3.left = r5;
+//        r3.right = r6;
+//        r6.left = r7;
+//        r6.right = r8;
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        t1.left = t2;
+        t1.right = t3;
+
+        TreeNode r1 = new TreeNode(1);
+        TreeNode r2 = new TreeNode(2);
+        TreeNode r3 = new TreeNode(3);
+        r1.left = r2;
+        r2.left = r3;
+        System.err.println(flipEquiv2(t1, r1));
     }
 
 }
