055803859a5d5400252a3a687b6e1329bd7d521c
niugiaogiao <zihaoAK47@163.com>
2022-10-04 20:39:14 +0800

    Add impl lc.637

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree637.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree637.java
@@ -1,5 +1,6 @@
 package com.niugiaogiao.binarytree.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -40,7 +41,7 @@ public class BinaryTree637 {
         while (!queue.isEmpty()) {
             int size = queue.size();
             double sum = 0;
-            for (int i = 0 ; i < size ; ++i) {
+            for (int i = 0; i < size; ++i) {
                 TreeNode item = queue.poll();
                 sum += item.val;
                 if (item.left != null) {
@@ -56,7 +57,36 @@ public class BinaryTree637 {
         return res;
     }
 
+    List<Double> result = new ArrayList<>();
+    List<Integer> count = new ArrayList<>();
+    List<Double> avg = new ArrayList<>();
+    public List<Double> averageOfLevelsDFS(TreeNode root) {
+        if (root == null) return result;
+        run(root, 0);
+        for (int i = 0 ; i < result.size() ; ++i) {
+            avg.add(result.get(i) / count.get(i));
+        }
+        return avg;
+    }
+
+    public void run(TreeNode root, int index) {
+        if (root == null) return;
+        if (index < result.size()) {
+            result.set(index, result.get(index) + root.val);
+            count.set(index, count.get(index) + 1);
+        } else {
+            result.add(root.val * 1.0);
+            count.add(1);
+        }
+
+        run(root.left, index + 1);
+        run(root.right, index + 1);
+    }
+
     public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        System.err.println(list.size());
+
         TreeNode t1 = new TreeNode(3);
         TreeNode t2 = new TreeNode(9);
         TreeNode t3 = new TreeNode(20);
@@ -66,6 +96,5 @@ public class BinaryTree637 {
         t1.right = t3;
         t3.left = t4;
         t3.right = t5;
-        averageOfLevels(t1);
     }
 }
