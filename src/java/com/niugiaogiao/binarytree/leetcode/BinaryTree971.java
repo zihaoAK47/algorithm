cd9d978753a83118250e12c7e9a5a1bbe7615fd1
niugiaogiao <zihaoAK47@163.com>
2022-10-08 23:04:56 +0800

    Add impl lc.971

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree971.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree971.java
@@ -1,7 +1,6 @@
 package com.niugiaogiao.binarytree.leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -40,26 +39,50 @@ public class BinaryTree971 {
 
     static int index = 0;
     static List<Integer> res = new LinkedList<>();
-    public static List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
-        return run(root, voyage) ? res : null;
+
+    public static List<Integer> flipMatchVoyageDFS1(TreeNode root, int[] voyage) {
+        return run1(root, voyage) ? res : null;
     }
 
-    public static boolean run(TreeNode node, int[] voyage) {
+    public static boolean run1(TreeNode node, int[] voyage) {
         if (node == null) return true;
         int tIndex = index;
         if (voyage[index++] != node.val) return false;
-        if (run(node.left, voyage) && run(node.right, voyage)) return true;
+        if (run1(node.left, voyage) && run1(node.right, voyage)) return true;
         index = tIndex + 1;
-        boolean leftRes = run(node.right, voyage);
-        boolean rightRes = run(node.left, voyage);
+        boolean leftRes = run1(node.right, voyage);
+        boolean rightRes = run1(node.left, voyage);
         if (leftRes && rightRes) res.add(node.val);
-
         return leftRes && rightRes;
     }
 
-    public static void main(String[] args) {
-        int[] data = new int[]{1, 2, 3};
-        flipMatchVoyage(null, data);
+
+    static List<Integer> res1 = new LinkedList<>();
+    static int index1 = 0;
+    public static List<Integer> flipMatchVoyageDFS2(TreeNode root, int[] voyage) {
+        run(root, voyage);
+        if (!res1.isEmpty() && res1.get(0) == -1) {
+            res1.clear();
+            res1.add(-1);
+        }
+        return res1;
     }
 
+    public static void run(TreeNode node, int[] voyage) {
+        if (node != null) {
+            if (node.val != voyage[index1++]) {
+                res1.clear();
+                res1.add(-1);
+                return;
+            }
+            if (index1 < voyage.length && node.left != null && node.left.val != voyage[index1]) {
+                res1.add(node.val);
+                run(node.right, voyage);
+                run(node.left, voyage);
+            } else {
+                run(node.left, voyage);
+                run(node.right, voyage);
+            }
+        }
+    }
 }
