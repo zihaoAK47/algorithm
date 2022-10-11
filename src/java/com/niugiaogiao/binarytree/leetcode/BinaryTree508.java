71c044dc40bccf8f42116714835e79f7849df71c
niugiaogiao <zihaoAK47@163.com>
2022-10-11 21:37:35 +0800

    Ac lc.508

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree508.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree508.java
@@ -26,21 +26,47 @@ public class BinaryTree508 {
         }
     }
 
-    List<Integer> res = new LinkedList<>();
-    Map<Integer, Integer> dict = new HashMap<>();
+   static Map<Integer, Integer> dict = new HashMap<>();
 
-    public int[] findFrequentTreeSum(TreeNode root) {
+    public static int[] findFrequentTreeSum(TreeNode root) {
+        run(root);
+        int maxCount = 0;
+        List<Integer> res = new LinkedList<>();
+        for(Map.Entry<Integer, Integer> item : dict.entrySet()) {
+            if (maxCount < item.getValue()) {
+                res.clear();
+                maxCount = item.getValue();
+            }
+            res.add(item.getKey());
+        }
 
-        return null;
+        int[] returnRes = new int[res.size()];
+        for (int i = 0 ; i < res.size() ; ++i) {
+            returnRes[i] = res.get(i);
+        }
+
+        return returnRes;
     }
 
-    public void run(TreeNode node) {
-        if (node == null || node.left == null && node.right == null) return;
+    public static int run(TreeNode node) {
+        if (node == null) return 0;
 
-        run(node.left);
-        run(node.right);
+        int leftR = run(node.left);
+        int rightR = run(node.right);
+        int res = node.val;
+        res += leftR + rightR;
+        dict.put(res, dict.getOrDefault(res, 0) + 1);
+        return res;
+    }
 
-
-
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(5);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(-5);
+        TreeNode t4 = new TreeNode(1);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        findFrequentTreeSum(t1);
     }
 }
