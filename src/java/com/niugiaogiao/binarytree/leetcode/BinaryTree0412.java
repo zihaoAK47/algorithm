32a601bcf04bf1a290c522a003fa69fc889324e1
niugiaogiao <zihaoAK47@163.com>
2022-10-17 17:16:42 +0800

    Ac lc.0412

---
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree0412.java
@@ -0,0 +1,45 @@
+package com.niugiaogiao.binarytree.leetcode;
+
+import com.niugiaogiao.binarytree.struct.TreeNode;
+
+/**
+ * 面试题 04.12. 求和路径
+ * <p>
+ * 给定一棵二叉树，其中每个节点都含有一个整数数值(该值或正或负)。设计一个算法，打印节点数值总和等于某个给定值的所有路径的数量
+ * 注意，路径不一定非得从二叉树的根节点或叶节点开始或结束，但是其方向必须向下(只能从父节点指向子节点方向)。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/paths-with-sum-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zihao
+ */
+public class BinaryTree0412 {
+
+    static int count = 0;
+
+    public static int pathSum(TreeNode root, int sum) {
+        if (root != null) {
+            dfs(root, sum, 0);
+            pathSum(root.left, sum);
+            pathSum(root.right, sum);
+            return count;
+        }
+        return count;
+    }
+
+    public static void dfs(TreeNode node, int sum, int t) {
+        if (node == null) return;
+        if (t + node.val == sum) {
+            count++;
+        }
+        dfs(node.left, sum, t + node.val);
+        dfs(node.right, sum, t + node.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        pathSum(null, 0);
+        System.err.println(count);
+    }
+}
---
+++ src/java/com/niugiaogiao/binarytree/struct/TreeNode.java
@@ -0,0 +1,11 @@
+package com.niugiaogiao.binarytree.struct;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
