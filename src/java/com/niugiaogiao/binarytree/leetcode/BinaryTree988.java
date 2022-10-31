f8f306b61699796074442f3a90c1f222fd535797
niugiaogiao <zihaoAK47@163.com>
2022-10-31 23:41:35 +0800

    Format 988

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree988.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree988.java
@@ -24,28 +24,28 @@ public class BinaryTree988 {
     static StringBuilder stringBuilder = new StringBuilder();
 
     public static String smallestFromLeaf(TreeNode root) {
-        String run = run1(root, 1);
-        System.err.println(run);
-        return "";
+        String run = run1(root);
+        return run;
     }
 
-    public static String run1(TreeNode node, int deep) {
-        if (node == null) return "-1";
-        String left = run1(node.left, deep + 1);
-        String right = run1(node.right, deep + 1);
-        int leftVal = Integer.parseInt(left);
-        int rightVal = Integer.parseInt(right);
-        int minVal = node.val;
-        if (!"-1".equals(left) && !"-1".equals(right)) {
-            minVal = Math.min(leftVal * 10 + node.val, rightVal * 10 + node.val);
+    public static String run1(TreeNode node) {
+        if (node == null) return "";
+        String left = run1(node.left);
+        String right = run1(node.right);
+        String minVal = ((char) (node.val + 'a')) + "";
+        if (!"".equals(left) && !"".equals(right)) {
+            left = left.concat(minVal);
+            right = right.concat(minVal);
+            return left.compareTo(right) >= 1 ? right  : left;
         } else {
-            if (!"-1".equals(left)) {
-                minVal = leftVal * 10 + node.val;
-            } else if (!"-1".equals(right)) {
-                minVal = rightVal * 10 + node.val;
+            if (!"".equals(left)) {
+                return left.concat(minVal);
+            } else if (!"".equals(right)) {
+                return right.concat(minVal);
             }
         }
-        return String.valueOf(minVal);
+
+        return minVal;
     }
 
     public static int run(TreeNode node, int deep) {
@@ -66,21 +66,13 @@ public class BinaryTree988 {
     }
 
     public static void main(String[] args) {
-        TreeNode t1 = new TreeNode(2);
-        TreeNode t2 = new TreeNode(1);
-        TreeNode t3 = new TreeNode(0);
+        TreeNode t1 = new TreeNode(4);
+        TreeNode t2 = new TreeNode(0);
+        TreeNode t3 = new TreeNode(1);
         TreeNode t4 = new TreeNode(1);
-        TreeNode t5 = new TreeNode(4);
-        TreeNode t6 = new TreeNode(1);
-        TreeNode t7 = new TreeNode(0);
-        TreeNode t8 = new TreeNode(0);
         t1.left = t2;
         t1.right = t3;
         t2.left = t4;
-        t3.left = t5;
-        t3.right = t6;
-        t5.left = t7;
-        t5.right = t8;
         smallestFromLeaf(t1);
     }
 }
