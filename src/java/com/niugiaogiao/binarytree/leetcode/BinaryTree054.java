f8eed5ffd9e7fb451d05c5c24b9da710bee279a3
niugiaogiao <zihaoAK47@163.com>
2022-11-08 09:46:57 +0800

    Add impl lc.054

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree054.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree054.java
@@ -26,4 +26,38 @@ public class BinaryTree054 {
         convertBST(root.left);
         return root;
     }
+
+    public TreeNode convertBSTMorris(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+
+        while (node != null) {
+            if (node.right == null) {
+                sum += node.val;
+                node.val = sum;
+                node = node.left;
+            } else {
+                TreeNode succ = getSuccessor(node);
+                if (succ.left == null) {
+                    succ.left = node;
+                    node = node.right;
+                } else {
+                    succ.left = null;
+                    sum += node.val;
+                    node.val = sum;
+                    node = node.left;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public TreeNode getSuccessor(TreeNode node) {
+        TreeNode succ = node.right;
+        while (succ.left != null && succ.left != node) {
+            succ = succ.left;
+        }
+        return succ;
+    }
 }
