5234bb2bb8cdff3da18399e3f9e4c8255ed5e575
niugiaogiao <zihaoAK47@163.com>
2022-11-13 19:30:51 +0800

    Ac lc.68_2

---
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree68_2.java
@@ -0,0 +1,32 @@
+package com.niugiaogiao.binarytree.leetcode;
+
+import com.niugiaogiao.binarytree.struct.TreeNode;
+
+/**
+ * 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ * https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/description/
+ *
+ * @author zi hao
+ * @version 1.0
+ * @date 2022-11-13 19:29
+ */
+public class BinaryTree68_2 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
+}
