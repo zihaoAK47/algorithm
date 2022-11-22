7d47a57061aaf37750cce15eea0133c887234cd4
niugiaogiao <zihaoAK47@163.com>
2022-11-22 18:30:47 +0800

    Format code.

--- src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree55_2.java
+++ src/java/com/niugiaogiao/binarytree/leetcode/BinaryTree55_2.java
@@ -3,6 +3,9 @@ package com.niugiaogiao.binarytree.leetcode;
 import com.niugiaogiao.binarytree.struct.TreeNode;
 
 /**
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树
+ * https://leetcode.cn/problems/ping-heng-er-cha-shu-lcof/
+ *
  * @author zi hao
  * @version 1.0
  * @date 2022-11-14 23:02
@@ -10,6 +13,7 @@ import com.niugiaogiao.binarytree.struct.TreeNode;
 public class BinaryTree55_2 {
 
     boolean res = true;
+
     public boolean isBalanced(TreeNode root) {
         if (root == null) {
             return true;
--- src/java/com/niugiaogiao/greedy/leetcode/Greedy179.java
+++ src/java/com/niugiaogiao/greedy/leetcode/Greedy179.java
@@ -2,6 +2,15 @@ package com.niugiaogiao.greedy.leetcode;
 
 import java.util.*;
 
+/**
+ * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+ * <p>
+ * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+ * https://leetcode.cn/problems/largest-number/
+ *
+ * @author zihao
+ */
+
 public class Greedy179 {
 
     static Set<Integer> pos = new HashSet<>();
